8623a2d354a21603f91f6c9cf4879bef75d79385
Madannadar <154582270+Madannadar@users.noreply.github.com>
2024-09-28 13:59:56 +0530

    adding backend

--- loginPage/loginpage/src/components/LoginSignUp/LoginSignUp.jsx
+++ loginPage/loginpage/src/components/LoginSignUp/LoginSignUp.jsx
@@ -6,10 +6,20 @@ import password_icon from '../Assets/password.png'
 
 export const LoginSignUp = () => {
 
+    // const [Name,setName] = useState('')
+    // const [email,setemail] = useState('')
+    // const [password,setpassword] = useState('')
+    // const [Confirmpassword,setConfirmpassword] = useState('')
     const [action,setAction] = useState("Sign Up");
 
+    // function handleSubmit(event){
+    //     event.preventDefault();
+    // }
+
   return (
+    
     <div className='SignUp_container'>
+        {/* <form onSubmit={{handleSubmit}}> */}
         <div className='header'>
             <div className='text'>{action}</div>
             <div className='underline'></div>
@@ -17,19 +27,27 @@ export const LoginSignUp = () => {
         <div className='inputs'>
             {action=== "Login"?<div></div>:<div className='input'>
                 <img src={user_icon} alt="" />
-                <input type="text" placeholder='Enter your name'/>
+                <input type="text" placeholder='Enter your name'
+                // onChange={e => setName(e.target.value)}
+                />
             </div>}
             <div className='input'>
                 <img src={email_icon} alt="" />
-                <input type="email" placeholder='Enter your email'/>
+                <input type="email" placeholder='Enter your email'
+                // onChange={e => setemail(e.target.value)}
+                />
             </div>
             <div className='input'>
                 <img src={password_icon} alt="" />
-                <input type="password" placeholder='Enter your password'/>
+                <input type="password" placeholder='Enter your password'
+                // onChange={e => setpassword(e.target.value)}
+                />
             </div>
             {action==="Login"?<div></div>:<div className='input'>
                 <img src={password_icon} alt="" />
-                <input type="Confirm-password" placeholder='Confirm your password'/>
+                <input type="Confirm-password" placeholder='Confirm your password'
+                // onChange={e => setConfirmpassword(e.target.value)}
+                />
             </div>}
             
         </div>
@@ -39,6 +57,7 @@ export const LoginSignUp = () => {
             <div className={action==="Login"?"Submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
             <div className={action==="Sign Up"?"Submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
         </div>
+    {/* </form> */}
     </div>
   )
 }
