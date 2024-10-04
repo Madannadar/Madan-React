7b0553a028e0b8e928df716a6671f751336ccfd0
Madannadar <154582270+Madannadar@users.noreply.github.com>
2024-10-04 19:46:55 +0530

    connection completed

--- Login with postgresql/server/database.js
+++ Login with postgresql/server/database.js
@@ -6,20 +6,24 @@ const pool = new Pool({
     password: "123",
     host: "localhost",
     port : 5432,
-    database: "madan21",
+    database: "a123", // idar tera database ka name table banane ke liye 
 });
 
-const createTblQry =  `CREATE TABLE accounts (
-    user_id serial PRIMARY KEY,
-    username VARCHAR ( 50 ) UNIQUE NOT NULL,
-    password VARCHAR ( 50 ) UNIQUE NOT NULL);`
+// all this commented code is to send query from this code to database 
 
-pool.query(createTblQry).then((Response) => {
-    console.log("table Created");
-    console.log(response);
-})
-.catch((err) => {
-    console.log(err);
-})
+
+// const createTblQry =  `CREATE TABLE accounts (
+//     user_id serial PRIMARY KEY,
+//     username VARCHAR ( 50 ) UNIQUE NOT NULL,
+//     password VARCHAR ( 50 ) UNIQUE NOT NULL);`
+
+
+// pool.query(createTblQry).then((Response) => {
+//     console.log("Database Created");
+//     console.log(response);
+// })
+// .catch((err) => {
+//     console.log(err);
+// })
 
 module.exports = pool;
--- Login with postgresql/server/server.js
+++ Login with postgresql/server/server.js
@@ -1,12 +1,30 @@
 const express = require("express")
 const cors = require("cors")
+const pool = require("./database")
 
 const app = express(); // app is the server with all functionality of express
 
 app.use(express.json()); // to handel json requeist 
 app.use(cors());
 
-app.get("/adduser", (req, res) => {
+app.post("/adduser", (req, res) => { // pahel it was get then post 
+    const username = req.body["username"]
+    const password =req.body["password"]
+
+    console.log("username:" + username);
+    console.log("password:", + password);
+
+    const insert = `INSERT INTO accounts ( username, password) VALUES ( '${username}', '${password}' );`
+    
+    pool.query(insert).then((response) => {
+        console.log("Data saved");
+        console.log(response);
+        
+    })
+    .catch((err) =>{
+        console.log(err);
+    })
+
     console.log(req.body);
     res.send("Response Received1: "+ req.body);
 });
